d244518381cd5e4f3c15cbadeb771eac9a90bbcf
banuakman <banu@asterya.com>
2021-02-04 23:19:51 -0500

    function created to return license badge

--- readme.md
+++ readme.md
@@ -1,29 +1,29 @@
-# 
-  License Badge
+# x
+  
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
-  ## Table of Contents
-  - [Descriptions](#Description)
-  - [Instructions](#Instructions)
-  - [Usage](#Usage)
-  - [Contributers](#Contributers)
-  - [Tests](#Tests)
-  - [Questions](#Questions)
+## Table of Contents
+- [Descriptions](#Description)
+- [Instructions](#Instructions)
+- [Usage](#Usage)
+- [Contributers](#Contributers)
+- [Tests](#Tests)
+- [Questions](#Questions)
   
-  ## Description
-  
-  
-  ## Instructions
-  
-  
-  ## Usage
-  
-  
-  ## Contributers
-  
-  
-  ## Tests
-  
-  
-  ## Questions
-  If you have any questions, please send an email to  or reach me through 
-  
+## Description
+x
+
+## Instructions
+x
+
+## Usage
+x
+
+## Contributers
+x
+
+## Tests
+x
+
+## Questions
+If you have any questions, please send an email to x or reach me through x
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,51 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "GNU GPL v3.0":
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    case "Mozilla Public License":
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    case "IBM Public License":
+      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+    case "None":
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  License Badge
+  
+${renderLicenseBadge(data.license)}
 
-  ## Table of Contents
-  - [Descriptions](#Description)
-  - [Instructions](#Instructions)
-  - [Usage](#Usage)
-  - [Contributers](#Contributers)
-  - [Tests](#Tests)
-  - [Questions](#Questions)
+## Table of Contents
+- [Descriptions](#Description)
+- [Instructions](#Instructions)
+- [Usage](#Usage)
+- [Contributers](#Contributers)
+- [Tests](#Tests)
+- [Questions](#Questions)
   
-  ## Description
-  ${data.description}
-  
-  ## Instructions
-  ${data.instructions}
-  
-  ## Usage
-  ${data.usage}
-  
-  ## Contributers
-  ${data.contributers}
-  
-  ## Tests
-  ${data.tests}
-  
-  ## Questions
-  If you have any questions, please send an email to ${data.email} or reach me through ${data.github}
-  `
+## Description
+${data.description}
+
+## Instructions
+${data.instructions}
+
+## Usage
+${data.usage}
+
+## Contributers
+${data.contributers}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please send an email to ${data.email} or reach me through ${data.github}`
 }
 
 module.exports = generateMarkdown;
